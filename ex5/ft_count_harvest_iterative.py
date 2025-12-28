0ee508d09ecb813c3979c418e33d8283719db3d5
Lahcen Sebar <lsebar@e1r12p3.1337.ma>
2025-12-28 13:06:13 +0100

    Python-00

--- ex5/ft_count_harvest_iterative.py
+++ ex5/ft_count_harvest_iterative.py
@@ -1,7 +1,6 @@
 def ft_count_harvest_iterative():
-	count = int(input("Days until harvest: "))
-	i = 1
-	for i in range(count):
-		print(f"Day {i}")
-	else:
-		print("Harvest time!")
+    count = int(input("Days until harvest: "))
+    for i in range(1, count + 1):
+        print(f"Day {i}")
+    else:
+        print("Harvest time!")
